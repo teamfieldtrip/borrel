2a0de1d16b3fda5bc4988b98b4c2105f12c8302b
Roelof Roos <github@roelof.io>
2017-03-09 13:35:49 +0100

    Added dummy file

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Main file, basically shouts that it's okay.
+ *
+ * Run this using `npm start`
+ *
+ * @author Roelof Roos <github@roelof.io>
+ */
+
+// Powered by http://trumpipsum.net/
+console.log('I know words. I have the best words.')
