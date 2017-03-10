920aec5b0c25099665fcfd4b12d9745711641940
Remco Schipper <github@remcoschipper.com>
2017-03-10 13:42:37 +0100

    Basic database functionality using SequelizeJS

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@
  *
  * @author Roelof Roos <github@roelof.io>
  */
+const async = require('async')
+const winston = require('winston')
+
+const database = require('./lib/database')
 
 // Powered by http://trumpipsum.net/
 console.log('I know words. I have the best words.')
+
+async.waterfall([database.boot], (error) => {
+  if (error) {
+    winston.error('Could not start: %s', error)
+    process.exit(1)
+  }
+})
 
 // TODO Setup database
 
