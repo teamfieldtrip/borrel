b5b590cde9f409e14a50a89921bc221f08596dec
Roelof Roos <github@roelof.io>
2017-03-14 15:56:57 +0100

    Improved post-install hooks
    Added proper name

--- bin/postinstall-hook.sh
+++ bin/postinstall-hook.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 ##
-## Adds Git Flow configs automatically
+## Does a thing before allowing you to send your thing to the thing
 ##
 ## @author Roelof Roos <github@roelof.io>
 ##
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GIT_DIR=$(dirname ${DIR})
-
-echo Setting Git Flow config
+cd $GIT_DIR
 
 set -xe
+
+node $GIT_DIR/src/build/build.js
+
+echo Setting Git Flow config
 
 git config --local gitflow.branch.master master
 git config --local gitflow.branch.develop develop
@@ -21,3 +24,15 @@ git config --local gitflow.prefix.hotfix hotfix/
 git config --local gitflow.prefix.support support/
 git config --local gitflow.prefix.versiontag v
 git config --local gitflow.path.hooks "${DIR}/.git/hooks"
+
+echo "Checking for global plugins..."
+if ! which sequelize > /dev/null; then
+    echo ""
+    echo "##############"
+    echo "# Attention! #"
+    echo "##############"
+    echo "You don't have sequelize-cli installed."
+    echo "Please install it:"
+    echo "> npm install -g sequelize-cli"
+    echo ""
+fi
