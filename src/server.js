71c51747f6a9ad6ad37c03be056b19c2f036d7df
Sven Boekelder <sven@plplpl.nl>
2017-03-14 13:04:08 +0100

    Changed let's into constants

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const http = require('http');
 const io = require('socket.io');
 
-let port = 14924;
+const port = 14924;
 
 // Create the server and set its response
-let server = http.createServer((req, res) => { 
-  res.writeHead(200,{ 'Content-Type': 'text/html' }); 
+const server = http.createServer((req, res) => {
+  res.writeHead(200,{ 'Content-Type': 'text/html' });
   res.end('<p style="font-family:monospace;">Listening..</p>');
 });
 
-let sock = io.listen(server);
+const sock = io.listen(server);
 
 sock.on('connection', function(socket){
 
