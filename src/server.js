2bab3da4a52515678d9862b259734a6750aad728
Sven Boekelder <sven@plplpl.nl>
2017-03-14 10:56:56 +0100

    General improvements for client.html and server.js
    - Updated client.html to fit server.js better
    - Added code comments
    - Changed let's into constants
    - Changed poll_data to pollData to fit eslint's requirements
    - Fixed the communication between server and client file

--- src/server.js
+++ src/server.js
@@ -1,38 +1,37 @@
-const http = require('http');
-const io = require('socket.io');
+const http = require('http')
+const io = require('socket.io')
 
-let port = 14924;
+const port = 14924
 
 // Create the server and set its response
-let server = http.createServer((req, res) => { 
-  res.writeHead(200,{ 'Content-Type': 'text/html' }); 
-  res.end('<p style="font-family:monospace;">Listening..</p>');
-});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end('<p style="font-family:monospace;">Listening..</p>')
+})
 
-let sock = io.listen(server);
+const sock = io.listen(server)
 
-sock.on('connection', function(socket){
-
+sock.on('connection', function (socket) {
   // emit an event to the socket
   socket.emit('request',
     () => {
-      console.log("Request")
+      console.log('Request')
     }
-  );
+  )
 
   // emit an event to all connected sockets
   socket.emit('broadcast',
     () => {
-      console.log("Broadcast")
+      console.log('Broadcast')
     }
-  );
+  )
 
   socket.on('poll',
-    (poll_data) => {
-      console.log(poll_data)
-    });
-});
+    (pollData) => {
+      console.log(pollData)
+    })
+})
 
 // Make server listen to the port
-server.listen(port);
-console.log("Server started on localhost:" + port);
+server.listen(port)
+console.log('Server started on localhost:' + port)
