8a0587472f24757d4614bf4d28e3d48853e1acf7
Remco Schipper <github@remcoschipper.com>
2017-03-22 13:57:44 +0100

    Changed the socket file

---
+++ src/lib/socket.js
@@ -0,0 +1,29 @@
+/**
+ * Handles the creation of the database connection
+ *
+ * @author Sven Boekelder <sven@plplpl.nl>
+ * @author Remco Schipper <github@remcoschipper.com>
+ */
+
+const http = require('http')
+const io = require('socket.io')
+const winston = require('winston')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(403, { 'Content-Type': 'text/html' })
+  res.end()
+})
+
+exports.connection = io.listen(server)
+exports.boot = function (callback) {
+  let onListening = () => {
+    server.removeListener('error', onError)
+    return callback(null)
+  }
+  let onError = (error) => {
+    server.removeListener('listening', onError)
+    winston.error('Socket error: %s', error)
+    return callback(error)
+  }
+  server.listen(process.env.SOCKET_PORT).once('listening', onListening).once('error', onError)
+}
