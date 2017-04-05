193e89bcdacae5191eef1368c994bedf446ab3ad
Roelof Roos <github@roelof.io>
2017-04-05 12:55:35 +0200

    Added better logging to lib/socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -10,11 +10,13 @@ const io = require('socket.io')
 const winston = require('winston')
 const socketioJwt = require('socketio-jwt')
 
+let listening = false
 const server = http.createServer((req, res) => {
   res.writeHead(403, { 'Content-Type': 'text/html' })
   res.end()
 })
 const socket = io.listen(server)
+
 // Firstly authenticate the client, afterwards create a data object to store user data
 socket.on('connection', socketioJwt.authorize({
   secret: process.env.JWT_SECRET,
@@ -24,9 +26,16 @@ socket.on('connection', socketioJwt.authorize({
 })
 
 exports.connection = socket
+
+// Expose boot method.
 exports.boot = function (callback) {
+  if (listening === true) {
+    throw new Error('Server already booted!')
+  }
   let onListening = () => {
     server.removeListener('error', onError)
+    winston.log('Server is listening')
+    listening = true
     return callback(null)
   }
   let onError = (error) => {
@@ -34,5 +43,16 @@ exports.boot = function (callback) {
     winston.error('Socket error: %s', error)
     return callback(error)
   }
-  server.listen(process.env.SOCKET_PORT).once('listening', onListening).once('error', onError)
+
+  let serverConfig = [
+    process.env.SOCKET_PORT || 8080,
+    process.env.SOCKET_HOST || 'localhost'
+  ]
+
+  winston.log('Server starting on %s:%d', serverConfig[1], serverConfig[0])
+
+  server
+    .listen(serverConfig[0], serverConfig[1])
+    .once('listening', onListening)
+    .once('error', onError)
 }
