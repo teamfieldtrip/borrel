da562c39d399a0fa45c16ad7473c8171c9ccd93b
Sven Boekelder <sven@plplpl.nl>
2017-04-11 12:24:14 +0200

    Lobby Model created

---
+++ src/model/lobby.js
@@ -0,0 +1,36 @@
+/**
+ * Lobby model
+ *
+ * @author Sven Boekelder
+ */
+
+module.exports = function (sequelize, DataTypes) {
+  const Lobby = sequelize.define('lobby', {
+    // Lobby ID
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      validate: {
+        noteEmpty: true,
+        isUUID: 4
+      }
+    }
+    // TODO Settings
+  }, {
+    comment: 'A lobby, contains players and settings.',
+    indexes: [
+      {fields: ['id'], unique: true}
+    ],
+    classMethods: {
+      associate: function (models) {
+        // The lobby always contains players
+        Lobby.hasMany(models.player, {foreignKey: 'lobby'})
+        // The lobby always has one host
+        Lobby.hasOne(models.player, {foreignKey: 'host'})
+      }
+    }
+  })
+
+  return Lobby
+}
