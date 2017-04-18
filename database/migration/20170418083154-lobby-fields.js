103bf9ae63e57a5d26c58135d67bc9efe1cde05b
Remco Schipper <github@remcoschipper.com>
2017-04-18 11:36:33 +0200

    Minor fixes

--- database/migration/20170418083154-lobby-fields.js
+++ database/migration/20170418083154-lobby-fields.js
@@ -1,41 +1,50 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
     queryInterface.addColumn('lobbies', 'duration', {
       type: Sequelize.INTEGER(3),
+      defaultValue: 1200,
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'amountOfPlayers', {
       type: Sequelize.INTEGER(2),
+      defaultValue: 4,
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'amountOfRounds', {
       type: Sequelize.INTEGER(2),
+      defaultValue: 1,
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'amountOfLifes', {
       type: Sequelize.INTEGER(2),
+      defaultValue: 1,
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'powerUpsEnabled', {
       type: Sequelize.BOOLEAN,
+      defaultValue: true,
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'centerLatitude', {
       type: Sequelize.STRING,
+      defaultValue: '52.36878505130836',
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'centerLongitude', {
       type: Sequelize.STRING,
+      defaultValue: '4.9003274738788605',
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'borderLatitude', {
       type: Sequelize.STRING,
+      defaultValue: '4.887398891150951',
       allowNull: false
     })
     queryInterface.addColumn('lobbies', 'borderLongitude', {
       type: Sequelize.STRING,
+      defaultValue: '52.35672342362662',
       allowNull: false
     })
   },
@@ -51,4 +60,4 @@ module.exports = {
     queryInterface.removeColumn('lobbies', 'borderLatitude')
     queryInterface.removeColumn('lobbies', 'borderLongitude')
   }
-};
+}
