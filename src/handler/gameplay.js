b2b5331f06d5530bb985d511174979ffb9b267dc
Sven Boekelder <sven@plplpl.nl>
2017-05-12 14:40:16 +0200

    Fixes for lint

--- src/handler/gameplay.js
+++ src/handler/gameplay.js
@@ -9,27 +9,26 @@ const EventEmitter = require('events').EventEmitter
 const winston = require('winston')
 const lodash = require('lodash')
 const database = require('../lib/database')
+
 const events = new EventEmitter()
-
-const create = function (data, callback) {
-
-}
 
 const setTargets = function (data, callback) {
   database.connection.models.lobby.findById(data.lobby.id).then((lobby) => {
     if (typeof lobby === 'undefined' || lobby === null) {
+      winston.error('Lobby not found')
       return callback('error_lobby_not_found')
     }
 
-    targets = lodash(lobby.players).groupBy('target')
-    targetCount = lodash.countBy(targets)
+    let targets = lodash(lobby.players).groupBy('target')
+    let targetCount = lodash.countBy(targets)
 
     for (let player of lobby.players) {
       // If the player does not have a target, assign one from the target object
       if (player.target == null) {
         for (let attempt of lobby.players) {
-          if (attempt.team != player.team && targetCount.target <= 2){
-            player.target = attempt.id;
+          if (attempt.team !== player.team && targetCount.target <= 2) {
+            player.target = attempt.id
+            winston.log('Assigned %s', attempt.id)
             callback(attempt.id)
             break
           }
@@ -39,4 +38,4 @@ const setTargets = function (data, callback) {
   })
 }
 
-
+module.exports = {events, setTargets}
