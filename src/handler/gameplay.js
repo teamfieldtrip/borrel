5bb6755667c941dd6fd40cf3e4058a83b72a3de7
Sven Boekelder <sven@plplpl.nl>
2017-05-12 15:43:38 +0200

    Fixed client parts

--- src/handler/gameplay.js
+++ src/handler/gameplay.js
@@ -13,19 +13,26 @@ const database = require('../lib/database')
 const events = new EventEmitter()
 
 const setTargets = function (data, callback) {
-  database.connection.models.lobby.findById(data.lobby.id).then((lobby) => {
+  database.connection.models.lobby.findById(data.lobbyId).then((lobby) => {
     if (typeof lobby === 'undefined' || lobby === null) {
       winston.error('Lobby not found')
       return callback('error_lobby_not_found')
     }
 
-    let targets = lodash(lobby.players).groupBy('target')
+    if (typeof lobby.players === 'undefined' || lobby.players === null) {
+      winston.error('Lobby doesn\'t have any players')
+      return callback('error_lobby_no_players')
+    }
+
+    let targets = lodash(lobby.Players).groupBy('target')
     let targetCount = lodash.countBy(targets)
+
+    console.log(lobby.players)
 
     for (let player of lobby.players) {
       // If the player does not have a target, assign one from the target object
       if (player.target == null) {
-        for (let attempt of lobby.players) {
+        for (let attempt of lobby.Players) {
           if (attempt.team !== player.team && targetCount.target <= 2) {
             player.target = attempt.id
             winston.log('Assigned %s', attempt.id)
