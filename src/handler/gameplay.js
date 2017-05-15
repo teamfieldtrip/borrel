66403f5878a79ea5be93b462f89254d69d5d474d
Roelof Roos <github@roelof.io>
2017-05-16 00:31:27 +0200

    Improved game target distribution

--- src/handler/gameplay.js
+++ src/handler/gameplay.js
@@ -13,8 +13,43 @@ const database = require('../lib/database')
 const events = new EventEmitter()
 
 /**
- * Sets targets for the players that don't have one assigned yet.
+ * Assigns all players that don't have a target a target
+ * @param  {Player[]} playerList List of players
+ * @return {Player[]}            List of players, all with targets
  */
+const assignTargets = (playerList) => {
+  if (typeof playerList !== 'object') { return false }
+
+  let t1 = lodash.filter(playerList, (ply) => { return ply.team === 1 })
+  let t2 = lodash.filter(playerList, (ply) => { return ply.team === 0 })
+
+  let assign = (sources, goals) => {
+    let targets = lodash.clone(goals)
+
+    sources.forEach((player) => {
+      targets.forEach((target) => {
+        if (player.target !== null) {
+          return
+        }
+
+        let filterCount = lodash.filter(sources, (ply) => {
+          return ply.target === target
+        }).length
+        if (filterCount < 2) {
+          player.target = target
+          console.log(`Assigning ${player.name} → ${target.name}`)
+        }
+      })
+      targets = lodash.shuffle(targets)
+    })
+  }
+
+  assign(t1, t2)
+  assign(t2, t1)
+
+  return playerList
+}
+
 const setTargets = function (data, callback) {
   database.connection.models.lobby.findById(data.lobbyId).then((lobby) => {
     if (typeof lobby === 'undefined' || lobby === null) {
@@ -26,25 +61,9 @@ const setTargets = function (data, callback) {
       winston.error('Lobby doesn\'t have any players')
       return callback('error_lobby_no_players')
     }
+    assignTargets(lobby.players)
 
-    let targets = lodash(lobby.Players).groupBy('target')
-    let targetCount = lodash.countBy(targets)
-
-    console.log(lobby.players)
-
-    lobby.players.forEach((player) => {
-      // If the player does not have a target, assign one from the target object
-      if (player.target == null) {
-        lobby.Players.some((attempt) => {
-          if (attempt.team !== player.team && targetCount.target <= 2) {
-            player.target = attempt.id
-            winston.log('Assigned %s', attempt.id)
-            callback(attempt.id)
-            return true
-          }
-        })
-      }
-    })
+    callback(null)
   })
 }
 
@@ -72,4 +91,4 @@ const tag = function (data, callback) {
   })
 }
 
-module.exports = {events, setTargets, tag}
+module.exports = {events, assignTargets, setTargets, tag}
